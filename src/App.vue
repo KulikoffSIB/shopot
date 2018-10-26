08febe9df15d5e9f143ff1d01bb8dc7c56538835
KulikovASe <KulikovASe@sibintek.ru>
2018-10-26 16:44:55 +0300

    mock server

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <store></store>
+        <image-uploader></image-uploader>
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import Store from "./components/Store";
+    import ImageUploader from "./components/image-uploader";
+
+    export default {
+        name: 'app',
+        components: {ImageUploader, Store},
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
