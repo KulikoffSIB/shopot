5050329d03dae6d661359e15bd93e96704af88d3
KulikovAS <KulikovASe@sibintek.ru>
2019-04-03 13:15:44 +0300

    del

--- src/directives/colorize.js
+++
@@ -1,9 +0,0 @@
-export default {
-    update(el, binding) {
-        if (binding.value > 100) {
-            el.classList.add('bg-red', 'fg-white');
-        } else {
-            el.classList.remove('bg-red', 'fg-white');
-        }
-    }
-}
